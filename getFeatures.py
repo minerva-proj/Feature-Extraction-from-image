9ac7d782c0a12c202c2abf8b127d526762f376e4
Minerva Sarma <81676380+minerva-proj@users.noreply.github.com>
2022-10-12 15:55:04 +0100

    Add files via upload

---
+++ getFeatures.py
@@ -0,0 +1,22 @@
+
+import cv2
+import pickle
+from tools.FeatureExtractor import FeatureExtractor
+from imutils import paths
+
+fe = FeatureExtractor()
+imagePaths = sorted(list(paths.list_images("D://try")))
+features = []
+
+# Extract the features for all the images
+print("Extracting features...")
+for i in range(len(imagePaths)):
+    image = cv2.imread(imagePaths[i])
+    ft = fe.extract(image)
+    features.append(ft)
+    print(f"Completed {i+1}/{len(imagePaths)}")
+
+# And dump to file
+pickle.dump(features, open("D:/data/features.pickle", "wb"))
+
+print("Done.")
